00d59ce846e4203e1cb31c1d79ac957aa7c02d16
Bijay Gautam <bijay.gautam1501@gmail.com>
2023-06-06 20:10:40 +0545

    question model created

---
+++ server/models/question.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const questionSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+
+	options: {
+		type: [{ id: String, value: String }],
+		required: true,
+	},
+
+	correct_option: {
+		type: { id: String, value: String },
+		required: true,
+	},
+
+	episode: {
+		type: Schema.Types.ObjectId,
+		ref: 'Episode',
+		required: true,
+	},
+
+	course: {
+		type: Schema.Types.ObjectId,
+		ref: 'Course',
+		required: true,
+	},
+});
+
+questionSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id;
+		returnedObject.correctOption = returnedObject.correct_option;
+
+		delete returnedObject._id;
+		delete returnedObject.__v;
+		delete returnedObject.correct_option;
+	},
+});
+
+const Question = model('Question', questionSchema);
+export default Question;
