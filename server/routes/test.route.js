b42830d980532e92fa0fdd7ad0ec730fb92c6e2e
Bijay Gautam <bijay.gautam1501@gmail.com>
2023-06-14 19:26:13 +0545

    test routes added

---
+++ server/routes/test.route.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import tests from '../controllers/test.controller.js';
+import result from '../controllers/result.controller.js';
+import isBodyATest from '../dtos/isBodyATest.js';
+import { isUser, isUserAdminOrTeacher } from '../middlewares/isUserRole.js';
+
+const testRouter = Router();
+
+// Get a test
+testRouter.get('/:episodeId/test/', tests.getOne);
+
+// Add a test
+testRouter.post('/:courseId/:episodeId/test/create', isUserAdminOrTeacher, isBodyATest, tests.create);
+
+// Edit a test
+testRouter.patch('/:episodeId/test/:testId/edit', isUserAdminOrTeacher, isBodyATest, tests.edit);
+
+// Remove a test
+testRouter.delete('/:episodeId/test/:testId/remove', isUserAdminOrTeacher, tests.remove);
+
+// Save results
+testRouter.post('/:testId/save-results', isUser, result.save);
+
+// Get results
+testRouter.get('/:testId/results', isUser, result.getOne);
+
+export default testRouter;
