088635812727776c143f7990f571096460398eb3
Bijay Gautam <bijay.gautam1501@gmail.com>
2023-06-30 19:53:58 +0545

    test layout updated

---
+++ client/src/views/test/index.jsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { useParams } from 'react-router-dom';
+import { TestContext } from 'src/components/test/context';
+import { testService } from 'src/services/test';
+import ModalContainer from 'src/components/common/modal/ModalContainer';
+import Modal from 'src/components/common/modal';
+import TestHeader from 'src/components/test/header';
+import TestForm from 'src/components/test/form';
+import './test.scss';
+
+const TEST_DEFAULT_VALUE = { questions: [], title: '' };
+
+export default function Test() {
+	const [test, setTest] = React.useState(TEST_DEFAULT_VALUE);
+	const [selectedOptions, setSelectedOptions] = React.useState([]);
+	const [showResults, setShowResults] = React.useState(false);
+	const [isLoading, setIsLoading] = React.useState(true);
+	const { episodeId, testId } = useParams();
+
+	React.useEffect(() => {
+		setIsLoading(true);
+
+		Promise.all([testService.getOne(episodeId), testService.getResults(testId)])
+			.then((response) => {
+				const [test, results] = response;
+
+				setTest(test);
+				if (!results) {
+					setSelectedOptions(test.questions.map((question) => ({ id: question.options[0]._id })));
+				}
+
+				if (results) {
+					setSelectedOptions(results.selectedOptions);
+					setShowResults(true);
+				}
+			})
+			.finally(() => setIsLoading(false));
+	}, [episodeId]);
+
+	const contextValue = {
+		test,
+		setTest,
+		selectedOptions,
+		setSelectedOptions,
+		showResults,
+		setShowResults,
+		isLoading,
+	};
+	return (
+		<TestContext.Provider value={contextValue}>
+			<div className="test-page">
+				<ModalContainer show>
+					<Modal close={false}>
+						<TestHeader />
+						<TestForm />
+					</Modal>
+				</ModalContainer>
+			</div>
+		</TestContext.Provider>
+	);
+}
